4f17c4db933ee3dc8f5a85d62123192e874cc533
melodywen <604989282@qq.com>
2022-01-07 15:30:18 +0800

    提交代码

--- servers/implement_servers/blog_implement_server.go
+++ servers/implement_servers/blog_implement_server.go
@@ -2,19 +2,48 @@ package implement_servers
 
 import (
 	"context"
-	"fmt"
 	"github.com/melodywen/go-providers/servers/gen/golang"
+	"github.com/melodywen/go-providers/servers/gen/golang/model"
 	"math/rand"
 )
 
 type BlogImplementServer struct {
 }
 
+func (b BlogImplementServer) Store(ctx context.Context, request *golang.BlogCreateRequest) (*golang.BlogIndexResponse, error) {
+	panic("implement me")
+}
+
+func (b BlogImplementServer) Update(ctx context.Context, request *golang.BlogIndexRequest) (*golang.BlogIndexResponse, error) {
+	panic("implement me")
+}
+
+func (b BlogImplementServer) Show(ctx context.Context, request *golang.BlogIndexRequest) (*golang.BlogIndexResponse, error) {
+	panic("implement me")
+}
+
+func (b BlogImplementServer) Delete(ctx context.Context, request *golang.BlogIndexRequest) (*golang.BlogIndexResponse, error) {
+	panic("implement me")
+}
+
 func NewBlogImplementServer() *BlogImplementServer {
 	return &BlogImplementServer{}
 }
 
+func mockBlogModel(size int) (response []*model.BlogModel) {
+	for i := 0; i < size; i++ {
+		response = append(response, &model.BlogModel{
+			Id:       int32(rand.Int()),
+			Title:    "",
+			Describe: "",
+			Author:   "",
+			Status:   2,
+		})
+	}
+	return response
+}
+
 func (b BlogImplementServer) Index(ctx context.Context, request *golang.BlogIndexRequest) (*golang.BlogIndexResponse, error) {
-	fmt.Println(1213131313)
-	return &golang.BlogIndexResponse{Status: int32(rand.Int())}, nil
+	data := mockBlogModel(int(request.Size))
+	return &golang.BlogIndexResponse{Data: data}, nil
 }
